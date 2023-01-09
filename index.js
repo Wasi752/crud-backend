0e5ce0fdd771b3ba95e883fe1a8689e78ce910d7
https://github.com/Wasi752 <osiur2@gmail.com>
2023-01-09 12:19:05 +0600

    CRUD Mobile

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const fs = require('fs')
+const { parse } = require('path')
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const { json } = require('express')
+const { stringify } = require('querystring')
+const { body, check, validationResult } = require('express-validator');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(express.static("public"))
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true, // <= Accept credentials (cookies) sent by the client
+}));
+
 
 app.get('/', (req, res) => {
     res.send('Get All Mobiles')
@@ -8,9 +23,39 @@ app.get('/', (req, res) => {
 app.get('/:id', (req, res) => {
     res.send('Get a Mobile by ID')
 })
-app.post('/', (req, res) => {
-    res.send('Create Mobile')
-})
+app.post('/',
+    check('price')
+        .isLength({ min: 4 })
+        .withMessage('price must be at least 4 digits')
+        .matches(/^[0-9]*$/)
+        .withMessage('must contain a number'),
+    check('config')
+        .isLength({ min: 20 })
+        .withMessage('must be at least 20 chars long'),
+    check('brandName')
+        .isLength({ min: 4 })
+        .withMessage('must be at least 4 chars long'),
+    check('modelName')
+        .isLength({ min: 5 })
+        .withMessage('must be exactly 4 digits long'),
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        fs.readFile('mobileData', 'utf8', (err, data) => {
+            const allData = JSON.parse(data)
+            const reqData = req.body;
+            const rawImageString = reqData.image.replace(/^data:image\/jpeg;base64,/, "");
+            const buffer = Buffer.from(rawImageString, "base64");
+            reqData.id = allData.mobileData.length + 1;
+            fs.writeFile(`public/mobilePicture/${reqData.id}.jpeg`, buffer, () => { });
+            reqData.image = `${reqData.id}.jpeg`;
+            allData.mobileData.push(reqData);
+            fs.writeFile("mobileData", JSON.stringify(allData), () => { });
+            res.send(JSON.stringify(reqData));
+        });
+    });
 app.put('/:id', (req, res) => {
     res.send('Update Mobile Data by ID')
 })
